f62d4708cf77e8907f1fc41fc2e69c27a8a5ed4c
manny <manuel.canarte@gmail.com>
2022-02-23 17:07:04 -0500

    Add task complete behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
-import { v4 as uuidv4 } from "uuid";
 import React, { useReducer, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
-type Item = { name?: string; id?: string };
-type Stuff = { items: Item[] };
+type Item = { name?: string; id?: string; isCompleted?: boolean };
+type List = { items: Item[] };
 
-const initialState: Stuff = { items: [] };
+const INITIAL_STATE: List = { items: [] };
 
-type Actions = "add" | "remove";
+const ADD = "add" as const;
+const REMOVE = "remove" as const;
+const TOGGLE = "toggle" as const;
 
-function reducer(state: Stuff, action: { type: Actions; payload: Item }) {
+const OPERATIONS = {
+  ADD,
+  REMOVE,
+  TOGGLE,
+};
+
+const actions = [ADD, REMOVE, TOGGLE];
+
+type Actions = typeof actions[number];
+
+function reducer(state: List, action: { type: Actions; payload: Item }): List {
   const { type, payload } = action;
 
   switch (type) {
-    case "add":
+    case ADD:
       let uniqueId = uuidv4();
-      return { items: [...state.items, { ...payload, id: uniqueId }] };
-    case "remove":
+      return {
+        items: [...state.items, { ...payload, id: uniqueId, isCompleted: false }],
+      };
+    case REMOVE:
       return { items: state.items.filter((item) => item.id !== payload.id) };
-
+    case TOGGLE:
+      const itemIndex = state.items.findIndex((item) => item.id === payload.id);
+      state.items[itemIndex].isCompleted = payload.isCompleted;
+      return {
+        items: state.items,
+      };
     default:
       return state;
   }
 }
 
 function App() {
-  const [{ items }, dispatch] = useReducer(reducer, initialState);
+  const [{ items }, dispatch] = useReducer(reducer, INITIAL_STATE);
 
   const [input, setInput] = useState<string | undefined>("");
 
@@ -34,14 +53,12 @@ function App() {
   const onEnter = (event: { key: string }) => {
     console.log("event key: ", event.key);
 
-    if (event.key === "Enter") {
-      if (input) {
-        dispatch({ type: "add", payload: { name: input } });
-      }
+    if (event.key === "Enter" && input) {
+      dispatch({ type: OPERATIONS.ADD, payload: { name: input } });
       cleanUp();
     }
   };
-
+  
   return (
     <>
       <div className="App">
@@ -59,24 +76,30 @@ function App() {
         />
         <ul>
           {items.length &&
-            items.map(({ name, id }, index: number) => (
+            items.map(({ name, id, isCompleted }) => (
               <li key={`${uuidv4()}`}>
-                  {name}
-                  - 
-                  <button
-                    onClick={() => {
-                      dispatch({ type: "remove", payload: { id } });
-                    }}
-                  >
-                    X
-                  </button>
+                <input
+                  type="checkbox"
+                  checked={isCompleted}
+                  onChange={(e) => {
+                    dispatch({ type: OPERATIONS.TOGGLE, payload: { id, isCompleted: e.target.checked} });
+                  }}
+                />
+                {isCompleted ? <s>{name}</s> : <span>{name}</span> }
+                <button
+                  onClick={() => {
+                    dispatch({ type: OPERATIONS.REMOVE, payload: { id } });
+                  }}
+                >
+                  X
+                </button>
               </li>
             ))}
         </ul>
         <button
           onClick={() => {
             if (input) {
-              dispatch({ type: "add", payload: { name: input } });
+              dispatch({ type: OPERATIONS.ADD, payload: { name: input } });
             }
             cleanUp();
           }}
